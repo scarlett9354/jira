32d9642563655acf0d5998f9a0fb55eaa1b95d5e
jinhaiyue <jinhaiyue@rocogz.com>
2021-07-28 17:33:41 +0800

    feat: 添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { cleanObject } from "utils"
+import { cleanObject, useDebounce, useMount } from "utils"
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import * as qs from "qs"
@@ -14,23 +14,24 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([])
 
   // 传入[]表示只在页面初始化时触发一次
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         // 说明请求成功了，还得添加一个table列表的state
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
+  const debouncedParam = useDebounce(param, 2000)
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         // 说明请求成功了，还得添加一个table列表的state
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam}></SearchPanel>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 // 排除value为0的情况
 export const isFalsy = (value) => value === 0 ? false : !value
 
@@ -11,3 +13,21 @@ export const cleanObject = (obj) => {
   })
   return result
 }
+
+// Custom Hook
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+  return debouncedValue
+} 
